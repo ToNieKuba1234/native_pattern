25fab69614f0ff25caf6fa075a8de3b999db41ee
Kasprovskyy <samsungbandzio@gmail.com>
2025-05-15 18:51:13 +0200

    Add files via upload

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-bold text-blue-500">
+        Hello from NativeWind
+      </Text>
+    </View>
+  );
+}
