f1e2ee46b5c5869398c2e641414aa5258208e95a
Jakub <jakub.kasprzak009@gmail.com>
2025-08-28 17:02:20 +0200

    Init commit

--- app/index.tsx
+++ app/index.tsx
@@ -2,10 +2,8 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-bold text-blue-500">
-        Hello from NativeWind
-      </Text>
+    <View>
+      <Text>Hello, World!</Text>
     </View>
   );
 }
